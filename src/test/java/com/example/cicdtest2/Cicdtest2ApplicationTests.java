51fd8144e3b2d681f960da0dda5a571a0d5f85f2
inswal843 <inswal843@gmail.com>
2024-11-06 14:40:16 +0900

    first

---
+++ src/test/java/com/example/cicdtest2/Cicdtest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdtest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
